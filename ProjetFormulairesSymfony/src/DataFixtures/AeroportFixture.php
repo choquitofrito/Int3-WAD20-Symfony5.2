71a2a4d38790139898fb37b3ba1e399893b1b43e
choquitofrito <zyriab@gmail.com>
2021-03-30 12:55:23 +0200

    ok axios

--- ProjetFormulairesSymfony/src/DataFixtures/AeroportFixture.php
+++ ProjetFormulairesSymfony/src/DataFixtures/AeroportFixture.php
@@ -1,9 +1,9 @@
 <?php
-
 namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+
 use Faker;
 use App\Entity\Aeroport;
 
@@ -12,15 +12,23 @@ class AeroportFixture extends Fixture
     public function load(ObjectManager $manager)
     {
 
-        
         $faker = Faker\Factory::create();
+    
         for ($i = 0; $i < 5; $i++) {
-            $aeroport = new Aeroport(['nom'=>$faker->city . " Airport",
-                                    'code'=>$faker->postcode,
-                                    'dateMiseEnService'=>$faker->dateTime,
-                                    'heureMiseEnService'=>$faker->dateTime,
-                                    'description'=>$faker->realText($faker->numberBetween(10,30))]);
             
+            // sans hydrate
+            // $aeroport = new Aeroport();
+            // $aeroport->setNom ($faker->city . " Airport");
+
+
+            // avec hydrate
+            $aeroport = new Aeroport([
+                'nom' => $faker->city . " Airport",
+                'code' => 'COD'. $i,
+                'dateMiseEnService' => $faker->dateTime,
+                'heureMiseEnService' => $faker->dateTime,
+                'description' => $faker->realText($faker->numberBetween(10, 30))
+            ]);
             $manager->persist($aeroport);
         }
         $manager->flush();
