2e0ce52d0f8440b0f85c05d8b66b51a44bf1508f
choquitofrito <zyriab@gmail.com>
2021-04-14 12:54:36 +0200

    instructions template

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'f2c3f1b2ee5ecdb48ed927c3c766a0ed2c90feec',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f2c3f1b2ee5ecdb48ed927c3c766a0ed2c90feec',
+    ),
+  ),
+);
